8eb6c258089eb1b7e9fa4fb2437792d84fe461f1
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-09-04 16:41:13 +0900

    actions を追加。ただしまだ動作は確認していない。

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 import { createStore } from 'redux';
+import { addTodo, toggleTodo, setVisibilityFilter } from './actions/index.js';
 
 let store = createStore(function(){ return 'Hello Redux'; });
-var contents = document.getElementById('contents');
-contents.innerHTML = store.getState().toString();
+
+//Todo の追加
+var addTodoElem = document.getElementById('addTodo');
+var input = addTodoElem.getElementsByTagName('input')[0];
+var button = addTodoElem.getElementsByTagName('button')[0];
+buton.addEventListener('click',()=>{
+    var todoText = input.value;
+    store.dispatch(addTodo(todoText));
+});
+
+
+//Todo の完了
+var todoList = document.getElementById('todoList');
+var elements = todoList.getElementsByTagName('li');
+var listArray = [...elements];
+listArray.forEach((v,index) => {
+    store.dispatch(toggleTodo(index));
+});
+
+//フィルタリング
+var links = document.getElementById('links');
+var childs = links.childNodes;
+var childList = [...childs];
+childList.filter(v => v.nodeName != '#text' ).forEach(v => {
+    v.addEventListener('click', e => {
+        var filterText = v.innerHTML;
+        store.dispatch(setVisibilityFilter(filterText));
+    });
+});
