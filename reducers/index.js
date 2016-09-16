59c0129f4d5beffb80ee2751fd281e1d2eeb0e8f
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-09-16 20:16:18 +0900

    Fix typo.

--- reducers/index.js
+++ reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 const todo = ( state, action ) => {
-    switch (action.typ) {
+    switch (action.type) {
     case 'ADD_TODO' :
         return {
             id: action.id,
